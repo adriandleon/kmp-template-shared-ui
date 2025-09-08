24929ee715a408d631548479c7728366c48a60dd
Adrian De León <adriandleon@gmail.com>
2025-09-08 16:41:10 -0600

    Test onboarding component

--- composeApp/src/commonTest/kotlin/com/example/project/common/di/TestModule.kt
+++ composeApp/src/commonTest/kotlin/com/example/project/common/di/TestModule.kt
@@ -10,26 +10,31 @@ import com.example.project.common.util.TestDispatcherProvider
 import com.example.project.common.util.testAnalytics
 import com.example.project.common.util.testLogger
 import com.example.project.logger.domain.Logger
-import com.example.project.onboarding.data.OnboardingRepositoryImpl
 import com.example.project.onboarding.domain.OnboardingRepository
 import dev.mokkery.MockMode.autofill
+import dev.mokkery.answering.returns
+import dev.mokkery.everySuspend
 import dev.mokkery.mock
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
-internal val testPlatformModule = module {}
-
 internal val testSharedModules = module {
     singleOf(::TestDispatcherProvider) { bind<DispatcherProvider>() }
     factoryOf(::DefaultStoreFactory) { bind<StoreFactory>() }
-    singleOf(::OnboardingRepositoryImpl) { bind<OnboardingRepository>() }
+    singleOf(::mockOnboardingRepository) { bind<OnboardingRepository>() }
     factory<Analytics> { testAnalytics }
     factory<Logger> { testLogger }
     factoryOf(::mockDataStore) { bind<DataStore<Preferences>>() }
 }
 
+internal val mockOnboardingRepository: OnboardingRepository =
+    mock(autofill) {
+        everySuspend { this@mock.allSlides() } returns emptyList()
+        everySuspend { this@mock.markOnboardingCompleted() } returns Unit
+    }
+
 internal val mockDataStore: DataStore<Preferences> = mock(autofill)
 
-internal val testModule = listOf(testPlatformModule, testSharedModules)
+internal val testModule = listOf(testSharedModules)
---
+++ composeApp/src/commonTest/kotlin/com/example/project/onboarding/presentation/component/OnboardingComponentTest.kt
@@ -0,0 +1,94 @@
+package com.example.project.onboarding.presentation.component
+
+import com.example.project.common.di.testModule
+import com.example.project.common.util.createComponentForTest
+import dev.mokkery.mock
+import io.kotest.core.extensions.Extension
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.koin.KoinExtension
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
+import org.koin.test.KoinTest
+
+class OnboardingComponentTest :
+    FunSpec({
+
+        test("should create component successfully") {
+            val component = createComponent()
+
+            component.shouldBeInstanceOf<OnboardingComponent>()
+            component.state.value shouldBe OnboardingComponent.OnboardingState()
+        }
+
+        test("should have correct initial state") {
+            val component = createComponent()
+
+            val initialState = component.state.value
+            initialState.currentSlide shouldBe 0
+            initialState.totalSlides shouldBe 0
+            initialState.isLoading shouldBe false
+            initialState.slides shouldBe emptyList()
+        }
+
+        test("should call navigation methods without errors") {
+            val component = createComponent()
+
+            // These should not throw exceptions
+            component.nextSlide()
+            component.previousSlide()
+            component.skipOnboarding()
+            component.completeOnboarding()
+        }
+
+        test("should handle multiple method calls") {
+            val component = createComponent()
+
+            // Call multiple methods in sequence
+            component.nextSlide()
+            component.previousSlide()
+            component.skipOnboarding()
+            component.completeOnboarding()
+            component.nextSlide()
+            component.previousSlide()
+
+            // Should not throw exceptions
+        }
+
+        test("should handle rapid successive calls") {
+            val component = createComponent()
+
+            // Make rapid successive calls
+            repeat(10) { component.nextSlide() }
+
+            // Should not throw exceptions
+        }
+
+        test("should maintain component state") {
+            val component = createComponent()
+
+            // Initial state should be consistent
+            val state1 = component.state.value
+            val state2 = component.state.value
+
+            state1 shouldBe state2
+        }
+
+        test("should handle empty slides gracefully") {
+            val component = createComponent()
+
+            val state = component.state.value
+            state.slides shouldBe emptyList()
+            state.totalSlides shouldBe 0
+        }
+    }),
+    KoinTest {
+    override val extensions: List<Extension> = listOf(KoinExtension(testModule))
+}
+
+private fun createComponent(onNavigateToHome: () -> Unit = mock()): OnboardingComponent =
+    createComponentForTest { componentContext ->
+        DefaultOnboardingComponent(
+            componentContext = componentContext,
+            onNavigateToHome = onNavigateToHome,
+        )
+    }
